1930dc714372228e41db1fa6719ebd7570edf139
Jonathan <vandersmissen.jonathan@gmail.com>
2022-12-04 14:21:33 +0100

    init exercice repo

---
+++ src/main/java/be/fgov/fahmp/technicalmeetup/TechnicalmeetupApplication.java
@@ -0,0 +1,13 @@
+package be.fgov.fahmp.technicalmeetup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechnicalmeetupApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechnicalmeetupApplication.class, args);
+	}
+
+}
---
+++ src/test/java/be/fgov/fahmp/technicalmeetup/TechnicalmeetupApplicationTests.java
@@ -0,0 +1,13 @@
+package be.fgov.fahmp.technicalmeetup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechnicalmeetupApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
