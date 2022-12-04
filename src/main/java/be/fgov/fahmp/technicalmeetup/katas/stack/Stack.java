6b064890dc5c7ebb01ab12d22cdc25df48c95753
Jonathan <vandersmissen.jonathan@gmail.com>
2022-12-04 15:45:20 +0100

    Katas : stack empty

---
+++ src/main/java/be/fgov/fahmp/technicalmeetup/katas/stack/Stack.java
@@ -0,0 +1,24 @@
+package be.fgov.fahmp.technicalmeetup.katas.stack;
+
+import org.springframework.stereotype.Component;
+
+/**
+ * @author John
+ * @created 04-12-22
+ * @project technicalmeetup
+ */
+@Component
+public class Stack {
+    // Empile une valeur
+    public void push(int value){}
+
+    // Récupére la valeur sur le dessus de pile et l’enlève de la pile
+    public Integer pop(){
+        return null;
+    }
+
+    // Récupére la taille de pile
+    public Integer count(){
+        return null;
+    }
+}
---
+++ src/test/java/be/fgov/fahmp/technicalmeetup/katas/stack/StackTest.java
@@ -0,0 +1,32 @@
+package be.fgov.fahmp.technicalmeetup.katas.stack;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+/**
+ * @author John
+ * @created 04-12-22
+ * @project technicalmeetup
+ */
+@SpringBootTest
+public class StackTest {
+
+    @Test
+    @DisplayName("Push should stake the provided value")
+    public void pushTest(){
+
+    }
+
+    @Test
+    @DisplayName("Pop should provide the latest value")
+    public void popTest(){
+
+    }
+
+    @Test
+    @DisplayName("Count should return the stak size")
+    public void countTest(){
+
+    }
+}
