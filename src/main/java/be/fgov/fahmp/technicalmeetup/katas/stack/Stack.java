e1f22aac8cee7415635e4dd361ada5b256382d91
Jonathan <vandersmissen.jonathan@gmail.com>
2023-04-05 11:11:33 +0200

    stack kata example

--- src/main/java/be/fgov/fahmp/technicalmeetup/katas/stack/Stack.java
+++ src/main/java/be/fgov/fahmp/technicalmeetup/katas/stack/Stack.java
@@ -2,6 +2,9 @@ package be.fgov.fahmp.technicalmeetup.katas.stack;
 
 import org.springframework.stereotype.Component;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * @author John
  * @created 04-12-22
@@ -9,16 +12,25 @@ import org.springframework.stereotype.Component;
  */
 @Component
 public class Stack {
+
+    List<Integer> value;
+
+    public Stack() {
+        this.value = new ArrayList<>();
+    }
+
     // Empile une valeur
-    public void push(int value){}
+    public void push(int value){
+        this.value.add(value);
+    }
 
     // Récupére la valeur sur le dessus de pile et l’enlève de la pile
     public int pop(){
-        return -1;
+        return this.value.remove(this.value.size() -1);
     }
 
     // Récupére la taille de pile
     public int count(){
-        return -1;
+        return this.value.size();
     }
 }
--- src/test/java/be/fgov/fahmp/technicalmeetup/katas/stack/StackTest.java
+++ src/test/java/be/fgov/fahmp/technicalmeetup/katas/stack/StackTest.java
@@ -2,7 +2,10 @@ package be.fgov.fahmp.technicalmeetup.katas.stack;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 /**
  * @author John
@@ -12,21 +15,46 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 public class StackTest {
 
+    @Autowired
+    Stack stack;
+
     @Test
     @DisplayName("Push should stake the provided value")
-    public void pushTest(){
+    public void pushTest42(){
+        stack.push(42);
+        int pop = stack.pop();
+        assertEquals(42, pop);
+
+    }
+
+    @Test
+    @DisplayName("Push should stake the provided value")
+    public void pushTest69(){
+        stack.push(69);
+        int pop = stack.pop();
+        assertEquals(69, pop);
 
     }
 
     @Test
     @DisplayName("Pop should provide the latest value")
     public void popTest(){
+        stack.push(42);
+        stack.push(69);
+        assertEquals(69, stack.pop());
+        assertEquals(42, stack.pop());
 
     }
 
     @Test
     @DisplayName("Count should return the stak size")
     public void countTest(){
+        stack.push(42);
+        assertEquals(1, stack.count());
+        stack.push(69);
+        assertEquals(2, stack.count());
+        stack.pop();
+        assertEquals(1, stack.count());
 
     }
 }
